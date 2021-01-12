45759fe0fd2aef0b948a8cfb537ee485703dd75e
PMacStew <23pmcclure@athenian.org>
2021-01-12 17:10:18 +0000

    finish shuffler

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,13 +1,29 @@
 public class MyMain {
 
-    // Shuffles an array, using the perfect shuffle algorithm
-    public static int[] perfectShuffle(int[] arr) { 
-        return null;
+
+
+
+    // Shuffles an
+    public static int[] perfectShuffle(int[] arr) {
+        int[] cards = new int[arr.length];
+        for (int i = 0; i < arr.length / 2; i++) {
+            cards[i * 2] = arr[i];
+            cards[(i * 2) + 1] = arr[(arr.length / 2) + i];
+        }
+        return cards;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
-    public static int[] selectionShuffle(int[] arr) { 
-        return null;
+    public static int[] selectionShuffle(int[] arr) {
+        int temp = 0;
+        int random = 0; 
+        for (int i = 0; i < arr.length; i++) {
+            random = (int) (Math.random() * arr.length);  
+            temp = arr[i];
+            arr[i] = arr[random];
+            arr[random] = temp;
+        }
+        return arr;
     }
 
 
